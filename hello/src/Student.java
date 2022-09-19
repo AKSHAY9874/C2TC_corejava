666535ea768008b6cfbc16c7310896a7cb615001
AKSHAY <AKSHAY@LAPTOP-EDFVRTQ0>
2022-09-19 12:51:09 +0530

    Assignment

--- hello/src/Student.java
+++ hello/src/Student.java
@@ -1,4 +1,8 @@
-
-public class Student {
-
-}
+package com.tns.assignment2;
+class Assignment_Student{  
+	//creating a default constructor  
+	Assignment_Student(){System.out.println("Student object is created");}  //main method  
+	public static void main(String args[]){  //calling a default constructor  
+		Assignment_Student b=new Assignment_Student();  
+	}  
+	}  
